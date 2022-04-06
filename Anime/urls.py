bd4f1aecabbad9367978ec821f480610af337e71
srinath <sarvasaisrinath.9@gmail.com>
2022-04-06 21:39:26 +0530

    second

--- Anime/urls.py
+++ Anime/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home)
+    path('home/',views.home),
     path('',views.home)
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
